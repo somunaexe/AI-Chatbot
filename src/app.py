4e3489036f927f2b5f13613ef583101593695968
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2025-08-25 16:43:25 +0100

    commit

--- src/app.py
+++ src/app.py
@@ -1,5 +1,18 @@
-import json
+import json, boto3, time
 
-def createChat():
+dynamo = boto3.resource("dynamodb")
+table = dynamo.Table("RMAI-Chat-Messages")
+
+def lambda_handler(event, context):
+    body = json.loads(event.get("body", "{}"))
+    message = body.get("message", "")
+
+    response = f"You said: {message}" if message else "Wagwan"
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"reply": response})
+    }
 
 
