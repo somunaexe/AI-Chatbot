63e77f8a806f862c8a875d0df7c524d605e31bfd
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2025-08-25 17:11:45 +0000

    added bedrock

--- src/app.py
+++ src/app.py
@@ -1,14 +1,39 @@
-import json, boto3, time
+import json, boto3, time, random, os
 
 dynamo = boto3.resource("dynamodb")
-table = dynamo.Table("RMAI-Chat-Messages")
+table = dynamo.Table(os.environ["TABLE_NAME"])
+
+bedrock = boto3.client("bedrock-runtime")
+MODEL_ID = os.environ["MODEL_ID"]
+
+def save_message(userId, role, text):
+    table.put_item(Item={
+        "userId": userId,
+        "timestamp": int(time.time()),
+        "role": role,
+        "message": text
+    })
+
+
+def get_messages(userId, limit=10):
+    resp = table.query(
+        KeyConditionExpress = "userId = :uid",
+        ExpressionAttributeValues = {":uid": userId},
+        Limit = limit
+    )
+    return resp["Items"]
+
 
 def lambda_handler(event, context):
     body = json.loads(event.get("body", "{}"))
-    message = body.get("message", "")
+    userId = body.get("userId", f"guest-{random.randint(100,900)}")
+    role = body.get("role", "sender")
+    user_message = body.get("message", "")
 
-    response = f"You said: {message}" if message else "Wagwan"
+    save_message(userId, "user", user_message)
+    save_message(userId, "bot", "Hello, I’m your bot!")
 
+    history = get_messages(userId)
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
